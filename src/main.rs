ab40a8b0dd96a901989c78a87a1c2f2d6b76a59a
rijkvp <rijk@rijkvp.nl>
2022-08-21 20:24:08 +0200

    Add proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,48 @@
 use clap::{Parser, ValueEnum};
 use owo_colors::OwoColorize;
 use std::{
-    fmt::{Debug, Display},
+    error::Error as StdError,
+    fmt::Debug,
     fs,
     io::{self, Read},
+    path::PathBuf,
 };
 use tera::{Context, Tera};
+use thiserror::Error;
 
 #[derive(Debug, Clone, ValueEnum)]
 enum FileFormat {
     JSON,
     YAML,
+    TOML,
+}
+
+fn deserialize(input: &str, format: FileFormat) -> Result<Context, Error> {
+    match format {
+        FileFormat::JSON => Ok(Context::from_serialize(
+            serde_json::from_str::<serde_json::Value>(&input).map_err(|e| {
+                Error::Deserialization(format!("Failed JSON deserialization: {}", e))
+            })?,
+        )?),
+        FileFormat::YAML => Ok(Context::from_serialize(
+            serde_yaml::from_str::<serde_json::Value>(&input).map_err(|e| {
+                Error::Deserialization(format!("Failed YAML deserialization: {}", e))
+            })?,
+        )?),
+        FileFormat::TOML => Ok(Context::from_serialize(
+            toml::from_str::<serde_json::Value>(&input).map_err(|e| {
+                Error::Deserialization(format!("Failed TOML deserialization: {}", e))
+            })?,
+        )?),
+    }
 }
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Args {
-    /// Read a file as input instead of stdin
-    #[clap(short, long)]
-    input: Option<String>,
+    /// Read a file as input
+    #[clap(short)]
+    input_path: Option<PathBuf>,
     /// File format of the input
     #[clap(value_enum)]
     format: FileFormat,
@@ -27,103 +51,54 @@ struct Args {
 }
 
 fn main() {
-    match run() {
-        Ok(result) => println!("{}", result),
-        Err(err) => {
-            eprintln!(
-                "{}{}{}",
-                err.r#type.red().bold(),
-                ": ".red().bold(),
-                err.msg
-            );
-        }
-    }
-}
-
-struct Error {
-    r#type: ErrorType,
-    msg: String,
-}
-
-impl Error {
-    fn new(r#type: ErrorType, msg: String) -> Self {
-        Self { r#type, msg }
-    }
-}
-
-enum ErrorType {
-    InputError,
-    TemplateLoad,
-    InputParse,
-    RenderError,
-}
-
-impl Display for ErrorType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ErrorType::InputError => f.write_str("input error"),
-            ErrorType::TemplateLoad => f.write_str("template load error"),
-            ErrorType::InputParse => f.write_str("input parse error"),
-            ErrorType::RenderError => f.write_str("template render error"),
-        }
-    }
-}
-
-fn run() -> Result<String, Error> {
     let args = Args::parse();
+    match run(args) {
+        Ok(result) => println!("{}", result),
+        Err(e) => {
+            eprintln!("{} {}", "error:".red().bold(), e.white())
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+enum Error {
+    #[error("IO: {0}")]
+    IO(String),
+    #[error("{0}")]
+    Deserialization(String),
+    #[error("Template render: {0}")]
+    Template(String),
+}
+
+impl std::convert::From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::IO(err.to_string())
+    }
+}
+
+impl std::convert::From<tera::Error> for Error {
+    fn from(err: tera::Error) -> Self {
+        if let Some(source) = err.source() {
+            Error::Template(format!("{}\n{}", err, source))
+        } else {
+            Error::Template(err.to_string())
+        }
+    }
+}
+
+fn run(args: Args) -> Result<String, Error> {
     let input = {
-        if let Some(input_file) = args.input {
-            fs::read_to_string(&input_file).map_err(|err| {
-                Error::new(
-                    ErrorType::InputError,
-                    format!("Failed to read file '{}': {}", input_file, err),
-                )
-            })
+        if let Some(input_path) = args.input_path {
+            fs::read_to_string(&input_path)?
         } else {
             let mut stdin = io::stdin();
             let mut input_buf = String::new();
-            stdin.read_to_string(&mut input_buf).map_err(|err| {
-                Error::new(
-                    ErrorType::InputError,
-                    format!("Failed to read stdin: {}", err),
-                )
-            })?;
-            Ok(input_buf)
-        }
-    }?;
-    let template_input = fs::read_to_string(&args.template).map_err(|err| {
-        Error::new(
-            ErrorType::TemplateLoad,
-            format!("Failed to load template '{}': {}", args.template, err),
-        )
-    })?;
-
-    let context = {
-        match args.format {
-            FileFormat::JSON => {
-                let json = serde_json::from_str::<serde_json::Value>(&input).map_err(|err| {
-                    Error::new(
-                        ErrorType::InputParse,
-                        format!("Failed to load JSON: {}", err),
-                    )
-                })?;
-                Context::from_serialize(json)
-                    .map_err(|_| Error::new(ErrorType::InputParse, String::from("???")))?
-            }
-            FileFormat::YAML => {
-                let yaml = serde_yaml::from_str::<serde_json::Value>(&input).map_err(|err| {
-                    Error::new(
-                        ErrorType::InputParse,
-                        format!("Failed to load YAML: {}", err),
-                    )
-                })?;
-                Context::from_serialize(yaml)
-                    .map_err(|_| Error::new(ErrorType::InputParse, String::from("???")))?
-            }
+            stdin.read_to_string(&mut input_buf)?;
+            input_buf
         }
     };
-    // (TEMPFIX) TODO: Add proper error messages
-    let result = Tera::one_off(&template_input, &context, false).unwrap();
+    let template_input = fs::read_to_string(&args.template)?;
+    let context = deserialize(&input, args.format)?;
+    let result = Tera::one_off(&template_input, &context, false)?;
     Ok(result)
 }
- 

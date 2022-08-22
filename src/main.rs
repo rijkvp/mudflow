97563e93928174d0952bb91f97eba7e841948259
rijkvp <rijk@rijkvp.nl>
2022-08-22 11:53:54 +0200

    Add template glob & output dir

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use owo_colors::OwoColorize;
 use std::{
     error::Error as StdError,
     fmt::Debug,
-    fs,
+    fs::File,
     io::{self, Read},
     path::PathBuf,
 };
@@ -12,25 +12,25 @@ use thiserror::Error;
 
 #[derive(Debug, Clone, ValueEnum)]
 enum FileFormat {
-    JSON,
-    YAML,
-    TOML,
+    Json,
+    Yaml,
+    Toml,
 }
 
 fn deserialize(input: &str, format: FileFormat) -> Result<Context, Error> {
     match format {
-        FileFormat::JSON => Ok(Context::from_serialize(
-            serde_json::from_str::<serde_json::Value>(&input).map_err(|e| {
+        FileFormat::Json => Ok(Context::from_serialize(
+            serde_json::from_str::<serde_json::Value>(input).map_err(|e| {
                 Error::Deserialization(format!("Failed JSON deserialization: {}", e))
             })?,
         )?),
-        FileFormat::YAML => Ok(Context::from_serialize(
-            serde_yaml::from_str::<serde_json::Value>(&input).map_err(|e| {
+        FileFormat::Yaml => Ok(Context::from_serialize(
+            serde_yaml::from_str::<serde_json::Value>(input).map_err(|e| {
                 Error::Deserialization(format!("Failed YAML deserialization: {}", e))
             })?,
         )?),
-        FileFormat::TOML => Ok(Context::from_serialize(
-            toml::from_str::<serde_json::Value>(&input).map_err(|e| {
+        FileFormat::Toml => Ok(Context::from_serialize(
+            toml::from_str::<serde_json::Value>(input).map_err(|e| {
                 Error::Deserialization(format!("Failed TOML deserialization: {}", e))
             })?,
         )?),
@@ -40,23 +40,23 @@ fn deserialize(input: &str, format: FileFormat) -> Result<Context, Error> {
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Args {
-    /// Read a file as input
+    /// Input source file
     #[clap(short)]
-    input_path: Option<PathBuf>,
+    input_source: Option<PathBuf>,
+    /// Output dir (required on template glob)
+    #[clap(short)]
+    output_dir: Option<PathBuf>,
     /// File format of the input
     #[clap(value_enum)]
     format: FileFormat,
-    /// Path to the template file
-    template: String,
+    /// Template file or glob
+    templates: String,
 }
 
 fn main() {
     let args = Args::parse();
-    match run(args) {
-        Ok(result) => println!("{}", result),
-        Err(e) => {
-            eprintln!("{} {}", "error:".red().bold(), e.white())
-        }
+    if let Err(e) = run(args) {
+        eprintln!("{} {}", "error:".red().bold(), e.white());
     }
 }
 
@@ -86,10 +86,10 @@ impl std::convert::From<tera::Error> for Error {
     }
 }
 
-fn run(args: Args) -> Result<String, Error> {
+fn run(args: Args) -> Result<(), Error> {
     let input = {
-        if let Some(input_path) = args.input_path {
-            fs::read_to_string(&input_path)?
+        if let Some(input_source) = args.input_source {
+            std::fs::read_to_string(&input_source)?
         } else {
             let mut stdin = io::stdin();
             let mut input_buf = String::new();
@@ -97,8 +97,28 @@ fn run(args: Args) -> Result<String, Error> {
             input_buf
         }
     };
-    let template_input = fs::read_to_string(&args.template)?;
     let context = deserialize(&input, args.format)?;
-    let result = Tera::one_off(&template_input, &context, false)?;
-    Ok(result)
+
+    if let Some(out_dir) = args.output_dir {
+        let mut tera = Tera::new(&args.templates)?;
+        tera.autoescape_on(vec![]);
+        std::fs::create_dir_all(&out_dir)?;
+        for template in tera.get_template_names() {
+            let path = out_dir.join(template);
+            if let Some(parent) = path.parent() {
+                std::fs::create_dir_all(parent)?;
+            }
+            let mut file = File::create(&path)?;
+            tera.render_to(template, &context, &mut file)?;
+            println!("{} {:<30} {:^7} {:>30}", "Rendered".green().bold(), template.dimmed(), "->".white().bold(), path.display().dimmed());
+        }
+    } else {
+        let template_input = std::fs::read_to_string(&args.templates)?;
+        let mut tera = Tera::default();
+        tera.autoescape_on(vec![]);
+        tera.add_raw_template(&args.templates, &template_input)?;
+        let mut stdout = io::stdout().lock();
+        tera.render_to(&args.templates, &context, &mut stdout)?;
+    }
+    Ok(())
 }
